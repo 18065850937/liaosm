9f00989193dc7dfd50e7ffb69068f14823b9ae37
MS-20171107HQVG\Administrator <997661236@qq.com>
2019-03-19 16:50:43 +0800

    增加src

---
+++ src/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/19 0019
+ * Time: 下午 2:41
+ */
+namespace Composertest;
+
+class test{
+    public function test(){
+        echo 'hello,liaoshimin';
+    }
+}
